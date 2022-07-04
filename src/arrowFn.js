e0daa391ff205fef6563fecdffed256c07105f97
Restyled.io <commits@restyled.io>
2022-07-04 06:35:53 +0000

    Restyled by clang-format

--- src/arrowFn.js
+++ src/arrowFn.js
@@ -6,9 +6,9 @@ const square = (number) => number * number;
 console.log(square(5));
 
 const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false },
+  {id : 1, isActive : true},
+  {id : 2, isActive : true},
+  {id : 3, isActive : false},
 ];
 
 // const activeJobs = jobs.filter(function (job) { return job.isActive; });
@@ -23,12 +23,14 @@ console.log(activeJobs);
 //     // In an old we used
 //     var self = this;
 //     setTimeout(function () {
-//       // This setTimeout callback function is not part of any objects. It is not like talk method in person object. It is a standalone function.
-//           
-//       // In case of callback function strict mode does not overwrite this behaviour. So, logs windows object. To solve this use self.
+//       // This setTimeout callback function is not part of any objects. It is
+//       not like talk method in person object. It is a standalone function.
+//
+//       // In case of callback function strict mode does not overwrite this
+//       behaviour. So, logs windows object. To solve this use self.
 //       console.log("this", this);
-// 
-//       console.log("self", self); 
+//
+//       console.log("self", self);
 //     }, 1000);
 //   },
 // };
@@ -36,9 +38,7 @@ console.log(activeJobs);
 const person = {
   talk() {
     //  Arrow function don't rebind this keyword. In automatically inherit this.
-    setTimeout(() => {
-      console.log("this", this);
-    }, 1000);
+    setTimeout(() => { console.log("this", this); }, 1000);
   },
 };
 
--- src/this.js
+++ src/this.js
@@ -1,17 +1,17 @@
 // In JS, this keyword always returns a reference to the current object.
 const person = {
-  name: "Amka",
-  walk() {
-    console.log(this);
-  },
+  name : "Amka",
+  walk() { console.log(this); },
 };
 
-// if we call a function as a method, in an object "this" always return a reference to that object
+// if we call a function as a method, in an object "this" always return a
+// reference to that object
 person.walk();
 
 // const walk = person.walk;
-// if we call a funcation as a standalone object or outside of an object this will return global object which is window object in browsers
-// if strict mode is enabled it is gonna return undefined instead of window object
+// if we call a funcation as a standalone object or outside of an object this
+// will return global object which is window object in browsers if strict mode
+// is enabled it is gonna return undefined instead of window object
 
 // BINDING THIS
 // With "bind" method we can set the value of "this"  permanently.
